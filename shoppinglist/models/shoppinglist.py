ea2a05604193382e9c1cc6844269727554f0f1d6
isheebo <kedgar751@gmail.com>
2017-09-30 11:41:19 +0300

    add the add_item method

--- shoppinglist/models/shoppinglist.py
+++ shoppinglist/models/shoppinglist.py
@@ -11,3 +11,13 @@ class ShoppingList:
         self.date_modified = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.items = dict()
         self.id_names = dict()
+
+    def add_item(self, item_id, item_name, price, quantity):
+        if item_name.title() in self.items:
+            print("Item is already on the list")
+            return False
+        new_item = Item(item_id, item_name, price, quantity)
+        self.items[new_item.name] = new_item
+        self.id_names[item_id] = new_item.name
+        self.date_modified = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return True
