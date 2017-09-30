1415651072b725f7d8e10ebd93056c5ed2b7b62f
isheebo <kedgar751@gmail.com>
2017-09-30 11:57:03 +0300

    add create_shoppinglist method

--- shoppinglist/models/user.py
+++ shoppinglist/models/user.py
@@ -11,3 +11,12 @@ class User:
         self.password = pwd_context.hash(password)
         self.shoppinglists = dict()  # {id:ShoppingList}
         self.ids_names = dict()  # mapping
+
+    def create_shoppinglist(self, list_id, name, notify_date):
+        if name.title() in self.shoppinglists:
+            print(f"a shoppinglist with name '{name}' already exists")
+            return False
+        new_list = ShoppingList(list_id, name, notify_date)
+        self.shoppinglists[new_list.name] = new_list
+        self.ids_names[new_list.id] = new_list.name
+        return True
