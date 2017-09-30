533d8e2786e3c786e3b9bbddb8cd7c834520b60a
isheebo <kedgar751@gmail.com>
2017-09-30 11:49:10 +0300

    add tests for edit_item

--- tests/test_shoppinglist.py
+++ tests/test_shoppinglist.py
@@ -44,3 +44,40 @@ class TestShoppingList(unittest.TestCase):
         self.assertEqual(len(self.shoppinglist.items), num_items + 2)
         self.assertTrue(self.shoppinglist.remove_item("abc457"))
         self.assertEqual(len(self.shoppinglist.items), num_items + 1)
+
+    def test_edit_item_fails_if_item_id_is_unknown(self):
+        num_items = len(self.shoppinglist.items)
+        self.assertTrue(self.shoppinglist.add_item("abc457", "cabbages", "5,000/=", "4"))
+        self.assertEqual(len(self.shoppinglist.items), num_items + 1)
+        self.assertTrue(self.shoppinglist.add_item("cdc671", "carrots", "3,000/=", "5"))
+        self.assertEqual(len(self.shoppinglist.items), num_items + 2)
+        self.assertFalse(self.shoppinglist.edit_item("cdc672", "green pepper", "2000", "6"))
+        self.assertEqual(len(self.shoppinglist.items), num_items + 2)
+
+        item = self.shoppinglist.items[self.shoppinglist.id_names["cdc671"]]
+        self.assertEqual(item.name, "Carrots", "Name is 'Green Pepper' and must be title-cased")
+        self.assertEqual(item.price, "3,000/=")
+        self.assertEqual(item.quantity, "5")
+
+    def test_edit_item_is_successful(self):
+        num_items = len(self.shoppinglist.items)
+        self.assertTrue(self.shoppinglist.add_item("abc457", "cabbages", "5,000/=", "4"))
+        self.assertEqual(len(self.shoppinglist.items), num_items + 1)
+        self.assertTrue(self.shoppinglist.edit_item("abc457", "carrots", "2,000/=", "5"))
+        self.assertEqual(len(self.shoppinglist.items), num_items + 1)
+
+        edited_item = self.shoppinglist.items[self.shoppinglist.id_names["abc457"]]
+        self.assertIsNotNone(edited_item)
+        self.assertEqual(edited_item.name, "Carrots")
+        self.assertEqual(edited_item.quantity, "5")
+        self.assertEqual(edited_item.price, "2,000/=")
+
+    def test_edit_item_fails_when_new_item_name_is_already_in_the_list(self):
+        num_items = len(self.shoppinglist.items)
+        self.assertTrue(self.shoppinglist.add_item("abc457", "cabbages", "5,000/=", "4"))
+        self.assertEqual(len(self.shoppinglist.items), num_items + 1)
+        self.assertTrue(self.shoppinglist.add_item("cdc671", "carrots", "3,00/=", "5"))
+        self.assertEqual(len(self.shoppinglist.items), num_items + 2)
+        self.assertTrue(self.shoppinglist.add_item("tad812", "green pepper", "2000", "6"))
+        self.assertEqual(len(self.shoppinglist.items), num_items + 3)
+        self.assertFalse(self.shoppinglist.edit_item("tad812", "carrots", "4000", "15"))
