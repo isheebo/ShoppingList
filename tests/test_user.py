2972f9090c892740ee9fb29de4575bab2c15373f
isheebo <kedgar751@gmail.com>
2017-09-30 11:52:38 +0300

    add tests for create_shoppinglist

--- tests/test_user.py
+++ tests/test_user.py
@@ -13,3 +13,16 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.user.name, "Alan Jackson")
         self.assertEqual(self.user.email, "jacko@gmail.com")
         self.assertTrue(pwd_context.verify("living-on-love", self.user.password))
+
+    def test_create_shoppinglist_fails_if_name_already_exists(self):
+        self.user.shoppinglists = dict(Groceries=ShoppingList("x2x2x2", "groceries", "17/02/2018"))
+        num_lists = len(self.user.shoppinglists)
+        self.assertFalse(self.user.create_shoppinglist("gh231a", "groceries", "5/12/2017"))
+        self.assertEqual(len(self.user.shoppinglists), num_lists)
+
+    def test_create_shoppinglist_is_successful(self):
+        num_lists = len(self.user.shoppinglists)
+        self.assertTrue(self.user.create_shoppinglist("gh231a", "groceries", "5/12/2017"))
+        self.assertEqual(len(self.user.shoppinglists), num_lists + 1)
+        self.assertTrue(self.user.create_shoppinglist("far712", "furniture", "12-12-2018"))
+        self.assertEqual(len(self.user.shoppinglists), num_lists + 2)
